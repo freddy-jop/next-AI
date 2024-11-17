4ba07f7091af26b258fa59af63481f7f8e0b28a1
freddy-jop <freddy.jopha.dev@gmail.com>
2024-11-17 20:16:24 +0100

    fix error buid with delete searchparams type

--- src/types/next.ts
+++ src/types/next.ts
@@ -5,7 +5,6 @@ export type LayoutParams<T extends Record<string, string | string[]>> = {
 
 export type PageParams<T extends Record<string, string | string[]>> = {
   params: Promise<T>;
-  searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export type RouteLayoutType = {
